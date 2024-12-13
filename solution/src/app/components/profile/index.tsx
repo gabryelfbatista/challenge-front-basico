aad842db141b1e3763be80dae1a813dfa31b7364
gabryelfbatista <gabryelfbatista@gmail.com>
2024-12-13 20:34:49 -0300

    profile picture now works and created a follow button

--- solution/src/app/components/profile/index.tsx
+++ solution/src/app/components/profile/index.tsx
@@ -1,21 +1,34 @@
 import Image from 'next/image'
+import profilePic from '../../images/profile_picture.jpeg'
 
 export const Profile = () => {
     return (
-        <div className="h-screen my-10 max-w-[800px] mx-auto bg-yellow-400 shadow rounded">
-            <div className=" h-60 bg-[url('./images/background_profile.jpg')] bg-cover bg-no-repeat "> 
-                <div className='mx-auto items-center justify-center flex w-[600px] h-[600px]'>
-                    <Image
-                        src='/images/profile_picture.jpeg'
-                        alt={'profile picture'}
-                        className="object-cover rounded-t-3xl"
-                        fill
-                        >
-                        </Image>   
+        <div>
+            <div className="max-h-[500px] mt-10 max-w-[800px] mx-auto bg-[url('./images/background_profile.jpg')] bg-contain bg-no-repeat shadow rounded">
+                <div className="grid grid-rows-2 h-full"> 
+                    <div className='flex flex-col space-y-4 place-items-center w-auto h-full'>
+                        <Image
+                            src={profilePic}
+                            alt={'profile picture'}
+                            className="object-cover rounded-full shadow-black mt-40"
+                            width={200}
+                            height={200} 
+                            >
+                        </Image>  
+                        <button className="bg-blue-500 text-white py-2 px-4 rounded-md">
+                            Follow
+                        </button>
+                        <div>Nome</div>
+                    </div>
+                    
+                </div>  
+            </div>
+            <div className="grid grid-cols-2 justify-between max-h-[400px] mt-2 max-w-[800px] mx-auto shadow rounded">
+                <div className="w-11/12 mr-4">
+                    OI
                 </div>
-                
-            </div>  
-
+                <div className="w-11/12 pl-4">oi</div>
+            </div>
         </div>
     )
 }
