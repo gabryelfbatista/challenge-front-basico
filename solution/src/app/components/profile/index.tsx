558649ae8d8a6dc38031f589c8e6d0d5d42f8158
gabryelfbatista <gabryelfbatista@gmail.com>
2024-12-14 19:34:00 -0300

    improved profile pictures and elements

--- solution/src/app/components/profile/index.tsx
+++ solution/src/app/components/profile/index.tsx
@@ -1,33 +1,45 @@
 import Image from 'next/image'
 import profilePic from '../../images/profile_picture.jpeg'
+import backgroundPic from '../../images/background_profile.jpg'
 
 export const Profile = () => {
     return (
-        <div>
-            <div className="max-h-[500px] mt-10 max-w-[800px] mx-auto bg-[url('./images/background_profile.jpg')] bg-contain bg-no-repeat shadow rounded">
-                <div className="grid grid-rows-2 h-full"> 
-                    <div className='flex flex-col space-y-4 place-items-center w-auto h-full'>
-                        <Image
-                            src={profilePic}
-                            alt={'profile picture'}
-                            className="object-cover rounded-full shadow-black mt-40"
-                            width={200}
-                            height={200} 
-                            >
-                        </Image>  
-                        <button className="bg-blue-500 text-white py-2 px-4 rounded-md">
-                            Follow
-                        </button>
-                        <div>Nome</div>
-                    </div>
-                    
-                </div>  
-            </div>
-            <div className="grid grid-cols-2 justify-between max-h-[400px] mt-2 max-w-[800px] mx-auto shadow rounded">
-                <div className="w-11/12 mr-4">
-                    OI
+        <div className="max-h-screen-lg">
+            <div className="justify-items-center max-h-[500px] mt-10 max-w-screen-lg mx-auto shadow rounded"> 
+                <div className="rounded-t-lg h-40">
+                    <Image 
+                        src={backgroundPic}
+                        alt={'background picture'}
+                        className="object-contain object-top w-full" 
+                    >
+                    </Image>
                 </div>
-                <div className="w-11/12 pl-4">oi</div>
+                <div className='relative mx-auto my-auto sm:h-20 sm:w-20 md:h-40 md:w-40 '>
+                    <Image
+                        src={profilePic}
+                        alt={'profile picture'}
+                        fill
+                        className="rounded-full object-cover object-center border-4 border-white shadow"
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+                        >
+                    </Image>  
+                </div>
+                <div className='flex flex-col justify-items-center my-5'>
+                    <button className="bg-blue-500 text-white mx-auto py-2 px-4 rounded-md">
+                        Follow
+                    </button>
+                    <div className='mx-auto my-5'>Nome</div>
+                    <div className='mx-auto mb-5'>Endereço</div>
+                </div>
+
+                    
+            </div>
+            <div className="grid md:grid-cols-2 justify-between max-h-[400px] mt-2 max-w-screen-lg mx-auto rounded">
+                <div className="flex-col max-h-full justify-sart pl-6 mr-1 shadow">
+                    <div>Personal info</div>
+                    <div>About</div>
+                </div>
+                <div className="flex-col max-h-full justify-sart pl-6 ml-1 shadow">oi</div>
             </div>
         </div>
     )
