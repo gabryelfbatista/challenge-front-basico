4c0d33f515f74e6f4005862a24a787da4a491a0d
gabryelfbatista <gabryelfbatista@gmail.com>
2024-12-16 23:59:18 -0300

    correctly removing eslint obligation

--- solution/eslint.config.mjs
+++ solution/eslint.config.mjs
@@ -7,14 +7,15 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  rules: {
-    "@typescript-eslint/no-explicit-any": "error"
-  }
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  })
 ];
 
 export default eslintConfig;
